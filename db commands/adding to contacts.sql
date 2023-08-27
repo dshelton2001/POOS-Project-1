810824f82d9b6390259488a8276d9b2fb70f716d
dshelton2001 <de690776@ucf.edu>
2023-08-27 17:46:35 -0400

    more contacts
    if you guys see this and have any ideas tell me now

--- db commands/adding to contacts.sql
+++ db commands/adding to contacts.sql
@@ -1,3 +1,34 @@
 -- Using this file to run lots of commands because typing in a console is absolutely boring
+insert into Contacts (FirstName, LastName, Phone, Email, UserID) VALUES ('Wacky', 'Guy', '5555555555', 'wackyguy23@gmail.com', '1');
+insert into Contacts (FirstName, LastName, Phone, Email, UserID) VALUES ('William', 'Afton', '1235554567', 'fivenights@freddies.org', '1');
+insert into Contacts (FirstName, LastName, Phone, Email, UserID) VALUES ('Sarah', 'Sell', '9415559832', 'seashell@seashore.com', '1');
 
-insert into Contacts (FirstName, LastName, Phone, Email, UserID) VALUES ('Dennis', 'Shelton', '2395553926', 'de690776@ucf.edu', '2');
+insert into Contacts (FirstName, LastName, Phone, Email, UserID) VALUES ('John', 'Zomboid', 'N/A', 'jzomboid@kentuckystate.gov', '2');
+insert into Contacts (FirstName, LastName, Phone, Email, UserID) VALUES ('Sonic', 'The', '1234567890', 'thehedgehog@urmums.house', '2');
+insert into Contacts (FirstName, LastName, Phone, Email, UserID) VALUES ('Steve', 'N/A', 'N/A', 'N/A', '2');
+insert into Contacts (FirstName, LastName, Phone, Email, UserID) VALUES ('Uncool', 'User', '6905554200', 'I am out of jokes', '2');
+
+insert into Contacts (FirstName, LastName, Phone, Email, UserID) VALUES ('Dennis', 'Shelton', '2395553926', 'dennis@ucf.edu', '4');
+insert into Contacts (FirstName, LastName, Phone, Email, UserID) VALUES ('Dennis', 'Shelton', '2395553926', 'dennis@ucf.edu', '5');
+insert into Contacts (FirstName, LastName, Phone, Email, UserID) VALUES ('Dennis', 'Shelton', '2395553926', 'dennis@ucf.edu', '6');
+insert into Contacts (FirstName, LastName, Phone, Email, UserID) VALUES ('Dennis', 'Shelton', '2395553926', 'dennis@ucf.edu', '7');
+
+insert into Contacts (FirstName, LastName, Phone, Email, UserID) VALUES ('Max', 'Max', '8005553833', 'max@ucf.edu', '3');
+insert into Contacts (FirstName, LastName, Phone, Email, UserID) VALUES ('Max', 'Max', '8005553833', 'max@ucf.edu', '5');
+insert into Contacts (FirstName, LastName, Phone, Email, UserID) VALUES ('Max', 'Max', '8005553833', 'max@ucf.edu', '6');
+insert into Contacts (FirstName, LastName, Phone, Email, UserID) VALUES ('Max', 'Max', '8005553833', 'max@ucf.edu', '7');
+
+insert into Contacts (FirstName, LastName, Phone, Email, UserID) VALUES ('Nathan', 'eksdee', '4205552517', 'nathan@ucf.edu', '3');
+insert into Contacts (FirstName, LastName, Phone, Email, UserID) VALUES ('Nathan', 'eksdee', '4205552517', 'nathan@ucf.edu', '4');
+insert into Contacts (FirstName, LastName, Phone, Email, UserID) VALUES ('Nathan', 'eksdee', '4205552517', 'nathan@ucf.edu', '6');
+insert into Contacts (FirstName, LastName, Phone, Email, UserID) VALUES ('Nathan', 'eksdee', '4205552517', 'nathan@ucf.edu', '7');
+
+insert into Contacts (FirstName, LastName, Phone, Email, UserID) VALUES ('Daniel', 'SyntaxError', '1115551111', 'Daniel@ucf.edu', '3');
+insert into Contacts (FirstName, LastName, Phone, Email, UserID) VALUES ('Daniel', 'SyntaxError', '1115551111', 'Daniel@ucf.edu', '4');
+insert into Contacts (FirstName, LastName, Phone, Email, UserID) VALUES ('Daniel', 'SyntaxError', '1115551111', 'Daniel@ucf.edu', '5');
+insert into Contacts (FirstName, LastName, Phone, Email, UserID) VALUES ('Daniel', 'SyntaxError', '1115551111', 'Daniel@ucf.edu', '7');
+
+insert into Contacts (FirstName, LastName, Phone, Email, UserID) VALUES ('Gadig', 'Gadig', '8882804331', 'Gadig@ucf.edu', '3');
+insert into Contacts (FirstName, LastName, Phone, Email, UserID) VALUES ('Gadig', 'Gadig', '8882804331', 'Gadig@ucf.edu', '4');
+insert into Contacts (FirstName, LastName, Phone, Email, UserID) VALUES ('Gadig', 'Gadig', '8882804331', 'Gadig@ucf.edu', '5');
+insert into Contacts (FirstName, LastName, Phone, Email, UserID) VALUES ('Gadig', 'Gadig', '8882804331', 'Gadig@ucf.edu', '6');
