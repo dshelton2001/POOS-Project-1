06cd2c80d0e7f1c464f8f627fc72b591b1ea2c6f
dshelton2001 <de690776@ucf.edu>
2023-08-27 17:15:49 -0400

    Adding SQL files to automate modifying the database
    We can keep doing this over and over until the website's finished

---
+++ db commands/adding to contacts.sql
@@ -0,0 +1,3 @@
+-- Using this file to run lots of commands because typing in a console is absolutely boring
+
+insert into Contacts (FirstName, LastName, Phone, Email, UserID) VALUES ('Dennis', 'Shelton', '2395553926', 'de690776@ucf.edu', '2');
