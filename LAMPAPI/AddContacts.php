55ab8a6fbeefa6d437066049bc34536f0dab680e
5yntaxError <63261805+5yntaxError404@users.noreply.github.com>
2023-09-04 20:51:09 -0400

    added registerUser, AddContacts currently erroring

---
+++ LAMPAPI/AddContacts.php
@@ -0,0 +1,49 @@
+<?php
+
+	$inData = getRequestInfo();
+
+	$fName = $inData["fName"];
+	$lName = $inData["lName"];
+	$phoneNum = $inData["phoneNum"];
+    $emailAdd = $inData["emailAdd"];
+    $userName = $inData["userName"];
+
+	$conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "COP4331");
+	if ($conn->connect_error) 
+	{
+		returnWithError( $conn->connect_error );
+	} 
+	else
+	{
+		$stmt = $conn->prepare("INSERT into Users (FirstName, LastName, Phone, Email, UserID) VALUES(?,?,?,?,?)");
+		$stmt->bind_param("sssss", $fName, $lName, $phoneNum, $emailAdd, $userName);
+		$stmt->execute();
+		$stmt->close();
+		$conn->close();
+		returnWithError("");
+	}
+
+	function getRequestInfo()
+	{
+		return json_decode(file_get_contents('php://input'), true);
+	}
+
+	function sendResultInfoAsJson( $obj )
+	{
+		header('Content-type: application/json');
+		echo $obj;
+	}
+	
+	function returnWithError( $err )
+	{
+		$retValue = '{"id":0,"firstName":"","lastName":"","error":"' . $err . '"}';
+		sendResultInfoAsJson( $retValue );
+	}
+	
+	function returnWithInfo( $searchResults )
+	{
+		$retValue = '{"results":[' . $searchResults . '],"error":""}';
+		sendResultInfoAsJson( $retValue );
+	}
+	
+?>
---
+++ LAMPAPI/registerUser.php
@@ -0,0 +1,48 @@
+<?php
+
+	$inData = getRequestInfo();
+
+	$fName = $inData["fName"];
+	$lName = $inData["lName"];
+	$userName = $inData["userName"];
+	$pwd = $inData["pwd"];
+
+	$conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "COP4331");
+	if ($conn->connect_error) 
+	{
+		returnWithError( $conn->connect_error );
+	} 
+	else
+	{
+		$stmt = $conn->prepare("INSERT into Users (FirstName, LastName, Username, Password) VALUES(?,?,?,?)");
+		$stmt->bind_param("ssss", $fName, $lName, $userName, $pwd);
+		$stmt->execute();
+		$stmt->close();
+		$conn->close();
+		returnWithError("");
+	}
+
+	function getRequestInfo()
+	{
+		return json_decode(file_get_contents('php://input'), true);
+	}
+
+	function sendResultInfoAsJson( $obj )
+	{
+		header('Content-type: application/json');
+		echo $obj;
+	}
+	
+	function returnWithError( $err )
+	{
+		$retValue = '{"id":0,"firstName":"","lastName":"","error":"' . $err . '"}';
+		sendResultInfoAsJson( $retValue );
+	}
+	
+	function returnWithInfo( $searchResults )
+	{
+		$retValue = '{"results":[' . $searchResults . '],"error":""}';
+		sendResultInfoAsJson( $retValue );
+	}
+	
+?>
